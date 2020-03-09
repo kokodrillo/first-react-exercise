4531b459154582bcc5541ce4c8bdf42058d92440
Maja Polanc <mayainstudio@gmail.com>
2020-03-09 21:40:03 +0100

    playing with different styles

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
  state = {
-   username: 'Majajam'
+   username: 'xxxxxx'
  }
-
 
 inputChangedHandler = (event) => {
   this.setState({username: event.target.value})
 }
 
 render() {
+  const style = {
+    margin: "16px auto",
+    border: "1px solid #eee",
+    cursor: "pointer",
+    boxshadow: "0 2px 3px #ccc",
+    width: "300px",
+    height: "150px",
+    textAlign: "center",
+    padding:"90px 50px ",
+    backgroundColor: "rgb(179, 176, 176)",
+    fontSize: "22px",
+    color: "white",
+  }
+
   return (
-    <div className ="App">
+    <div style={style} className ="App">
       < UserOutput
           userName={this.state.username}/>
 
